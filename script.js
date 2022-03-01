09f34ec8ccc916747b401cf2bab88a0d9ac8dc8a
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-01 16:13:29 +0100

    odd & even - need fixes

--- script.js
+++ script.js
@@ -37,3 +37,56 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+
+let userOddOrEven = prompt("How it will be the sum of the numbers, ODD or EVEN?");
+let userNumber = parseInt(prompt("Type your number, from 1 to 5"));
+
+document.writeln("Your choise: " + userOddOrEven);
+document.writeln("Your number: " + userNumber);
+
+let userEven;
+
+if (userOddOrEven.toLowerCase = "even") {
+    userEven = even;
+} else if (userOddOrEven.toLowerCase = "odd") {
+    userEven = false;
+}
+
+document.writeln(userEven);
+
+function getRandomNumber (min, max) {    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let pcNumber = getRandomNumber(1, 5);
+document.writeln("Pc number: " + pcNumber);
+
+function valueOddOrEven (value) {
+    if (value % 2 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let sum = userNumber + pcNumber;
+document.writeln("The sum of the numbers is: " + sum);
+
+
+let sumEven;
+
+if (valueOddOrEven(sum)) {
+    sumEven = true; 
+} else {
+    sumEven = false;
+}
+
+document.writeln(sumEven);
+
+
+if (sumEven == userEven) {
+    document.writeln("Congratulations, you guessed!")
+} else {
+    document.writeln("You nearly got it, maybe you'll get it next time :)")
+}
