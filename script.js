35482084cacf7dfac5cba65bb74c26906708fad5
xVal4k <cristianbricicaru.cb@gmail.com>
2022-03-01 14:57:34 +0100

    palidroma exercise

---
+++ script.js
@@ -0,0 +1,39 @@
+/* PALIDROMA */
+/*
+Chiedere all’utente di inserire una parola 
+Creare una  funzione per capire se la parola inserita
+è palindroma
+*/
+
+/*
+const word = prompt("Digit a word");
+
+for (let i = 0; i < word.length; i++) {
+    function isPalindrome(word) {
+        if (word.charAt(0) == word.charAt(word.lenght - 1) && word.charAt(i) == word.charAt(word.lenght - i)) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+}
+
+document.writeln("This is the word you typed: " + word);
+
+if (isPalindrome(word) == true) {
+    document.writeln("Your word is a palindrome word!");
+} else {
+    document.writeln("Your word isn't a palindrome word");
+}
+*/
+
+
+/* PARI E DISPARI */
+
+/*
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
